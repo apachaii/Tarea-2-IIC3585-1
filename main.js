1eb85dee7326be0c3b2cc94cf9a47ac6fd053876
Danilo <dnmolina@uc.cl>
2020-04-06 00:08:53 -0400

    v2

--- main.js
+++ main.js
@@ -1,59 +1,50 @@
-function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
-  }
 
   function rand(n){
     // creamos un numero al azar entre 1 y 10 (o cual sea la cantidad de imágenes)
-     return(Math.floor(Math.random() * n + 1 ));
+     return(Math.floor(Math.random() * n  ));
     }
     //guardas imagenes en el array
     let cambia_imagen = new Array();
     cambia_imagen[0] = "azul.png";
     cambia_imagen[1] = "verde.png";
 
-    
+    let cambia_psicion = new Array();
+    cambia_psicion[0] = 100;
+    cambia_psicion[1] = 250;
+    cambia_psicion[2]= 400;
 
-    
+    let otro=  document.getElementById("otro")
+    let ia = document.getElementById("ia")
+
     function myFunction() {
 
          doMove()
 
     }
-    
-    function cambiar(){
-         document.getElementById("ia").src = cambia_imagen[rand(2)-1];
-    }
+    function myFunction2() {
+
+      doMove2()
+
+ }
+
     let getRandomNumber = size => {
       return Math.floor(Math.random() * size);
     }
-    let bajar = size => {
- 
-    }
 
     function doMove() {
+      ia.style.marginLeft = cambia_psicion[rand(3)]+"px";
+      // ia.style.marginRight = cambia_psicion[rand(3)]+"px";
+      console.log(ia.style.marginLeft )
 
-      let elVirus = document.getElementById("ia");
-      elVirus.style.marginLeft = getRandomNumber(500)+"px";
-      elVirus.style.marginRight = getRandomNumber(500)+"px";
-      
-
-    //   for (let i = 500; i > 0; i--) {
-    //      let n =+ i;
-    //      let elVirus = document.getElementById("ia");
-    //      elVirus.style.marginTop =  n+"px";
-    //     console.log(n)
-    //  }
-      
   }
 
-  let $map = document.getElementById("ia");
-  let clicks = 0;
+  function doMove2() {
 
-  $map.addEventListener('click', function (e) {
-   console.log('click');
-   clicks++;
-   document.getElementById("puntos").innerHTML = clicks;
-});
+    otro.style.marginLeft =cambia_psicion[rand(3)]+"px";
+    otro.style.marginRight = cambia_psicion[rand(3)]+"px";
+
+}
+
 
 function bajara(n){
   return n-1;
@@ -63,28 +54,42 @@ function bajara(n){
 window.onload = (event) => {
 
   let px = 0
-   document.getElementById("puntos").innerHTML = clicks;
-   cambiar()
+  let px2 = 0
+  ia.src  = "azul.png"
+  otro.src = "verde.png";
+
   // setInterval('myFunction()',500)
  myFunction()
- let ff = document.getElementById("ia");
- let py = 0
- setInterval(()=>{
-  
-    py = px++
-   ff.style.marginTop =  py+"px";
+ myFunction2()
+ let py2 = 0
 
-  if(py === 740 ){
+ setInterval(()=>{
+    py2 = px2++
+   ia.style.marginTop =  py2+"px";
+
+  if(py2 === 640 ){
     myFunction()
-    ff.style.marginTop =  0+"px";
-   px = 0
-   py = 0
+    ia.style.marginTop =  0+"px";
+   px2 = 0
+   py2 = 0
   }
  },10)
 
+//  setInterval(()=>{
+//   py = px++
+//  otro.style.marginTop =  py+"px";
+
+// if(py === 640 ){
+//   myFunction2()
+//   otro.style.marginTop =  0+"px";
+//  px = 0
+//  py = 0
+// }
+// },10)
 
 
- 
+
+
 
 };
 
