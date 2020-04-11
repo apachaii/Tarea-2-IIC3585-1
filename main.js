30bf6baaa5de837e48b7a567ba2485e182562650
Danilo <dnmolina@uc.cl>
2020-04-11 14:10:22 -0400

    bug fix

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 let puntos = 0
 let n = 0;
-let cambia_tiempo = new Array();
 let cuadrado = document.getElementById("div1")
 let verde = document.getElementById("div2")
+let rojo = document.getElementById("div3")
 const subject = new Rx.Subject()
-cambia_tiempo[0] = 4;
-cambia_tiempo[1] = 3;
-cambia_tiempo[2] = 3;
-cambia_tiempo[3] = 1;
+let cambia_tiempo = new Array();
+cambia_tiempo[0] = 1000;
+cambia_tiempo[1] = 3000;
+cambia_tiempo[2] = 5000;
+
 let p = 0
 
-
+let red=0
+let blue =0
+let greeen =0
 
 function rand(n){
   return(Math.floor(Math.random() * n  ));
@@ -20,16 +23,22 @@ window.setInterval(function(){
   p++;
 },1000);
 
-
+rojo.style.animation = `mymove3`;
 cuadrado.style.animation = `mymove3`;
 verde.style.animation = `mymove3`;
+
     window.addEventListener('keypress', function(e){     
 
       let jugar = e.key;
       if(jugar === "j" ){
+        red =cambia_tiempo[rand(3)]
+        greeen =  cambia_tiempo[rand(3)]
+        blue = cambia_tiempo[rand(3)]
+
         
-        setTimeout(cuadradoStart, 5000);
-        setTimeout(verdeStart, 3000);
+        setTimeout(cuadradoStart, blue);
+        setTimeout(verdeStart,greeen);
+        setTimeout(rojoStart, red);
 
         window.setInterval(function(){
           n++;
@@ -50,50 +59,91 @@ verde.style.animation = `mymove3`;
       function verdeStart(){
         verde.style.animation = `` 
       }
+      function rojoStart(){
+        rojo.style.animation = `` 
+      }
+
 
        
 
        
        subject.subscribe((n) => {
         //cuadrado azul
+        t = 9+(blue/1000)
       window.addEventListener('keypress', function(e){
         let letra = e.key;
-        t = 9+5
+       
         console.log(n)
         if(letra === "z" && + n === t){
 
           cuadrado.style.animation = `mymove3`;
           puntos = puntos+1
-          console.log("puntos" +puntos)             
+          console.log("puntos" +puntos)     
+    
         }//fin z
                    
        });//fin 2
 
-        if (n === 15){
+        if (n === t){
         console.log("paso por aca")
         cuadrado.style.animation = `mymove3`;
         puntos = puntos-10
 
        }
       });
+
       subject.subscribe((n) => {                   
        //cuadrado verde
+       t = 9+(greeen/1000)
        window.addEventListener('keypress', function(e){
         let letra = e.key;
-        t = 9+3
+        
         console.log(n)
         if(letra === "x" && + n === t){
 
           verde.style.animation = `mymove3`;
           puntos = puntos+1
-          console.log("puntos"+puntos)             
+          console.log("puntos"+puntos)  
+         
         }//fin z
                    
        });//fin 2
+
+       if (n === t){
+        console.log("paso por aca")
+        verde.style.animation = `mymove3`;
+        puntos = puntos-10
+ 
+       }
        
       });
 
+      subject.subscribe((data) => {
+       //cuadrado rojo
+       t = 9+(red/1000)
+      window.addEventListener('keypress', function(e){
+        let letra = e.key;
+      
+        console.log(n)
+        if(letra === "c" && + n === t){
 
+          rojo.style.animation = `mymove3`;
+          puntos = puntos+1
+          console.log("puntos" +puntos)        
+  
+        }//fin z
+                   
+       });//fin 2
+
+        if (n === t){
+        console.log("paso por aca")
+        rojo.style.animation = `mymove3`;
+        puntos = puntos-10
+
+       }
+
+    });
+      
    window.onload = function() {
 
 
