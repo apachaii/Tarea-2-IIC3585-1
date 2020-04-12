fd4016f50b38ce5cbd9f6714dbfd8047986ec076
pablo <perojas3@uc.cl>
2020-04-12 11:52:11 -0400

    Ready with base gameplay

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const AMOUNT_OF_CHANNELS = 2;
-const TIME_BEFORE_PRESS = 5000; // in miliseconds
-const ERROR_MARGIN = 500; // in miliseconds
+const TIME_BEFORE_PRESS = 4500; // in miliseconds
+const TIME_BEFORE_DISSAPEAR = (TIME_BEFORE_PRESS * 10) / 9; // in miliseconds
+const ERROR_MARGIN = 200; // in miliseconds
+
+const FIRST_PLAYER_BLUE = "z";
+const FIRST_PLAYER_GREEN = "x";
+const SECOND_PLAYER_BLUE = "n";
+const SECOND_PLAYER_GREEN = "m";
 
 function build_play_area() {
   const juegos = document.getElementsByClassName("juego");
@@ -27,27 +33,97 @@ function build_play_area() {
 
 build_play_area();
 
-const start_moments = [1000, 1600, 3000, 3630, 4200, 6000];
-const press_moments = start_moments.map((moment) => moment + TIME_BEFORE_PRESS);
+// Game started
 
-const spawn_times = Rx.Observable.from(start_moments).flatMap((x) =>
-  Rx.Observable.timer(x).map((_) => x)
+let gameStart = Date.now();
+
+const spawn_moments = [1000, 1400, 1800, 2200, 2600, 3000, 3400];
+
+// Blue press moment
+
+let currentBluePress = { time: 0, players_pressed: [false, false] };
+
+const press_moments_emit = spawn_moments.map((currnt_value, index, array) => {
+  let emit_moment = 0;
+  if (index !== 0) {
+    emit_moment = array[index - 1] + TIME_BEFORE_PRESS + ERROR_MARGIN;
+  }
+  return {
+    emit_moment,
+    press_moment: currnt_value + TIME_BEFORE_PRESS,
+  };
+});
+
+Rx.Observable.from(press_moments_emit)
+  .flatMap((change_press_moment) =>
+    Rx.Observable.timer(change_press_moment.emit_moment).map(
+      (_) => change_press_moment.press_moment
+    )
+  )
+  .subscribe((press_moment) => {
+    currentBluePress = { time: press_moment, players_pressed: [false, false] };
+  });
+
+const spawn_times = Rx.Observable.from(spawn_moments).flatMap((spawn_moment) =>
+  Rx.Observable.timer(spawn_moment).map((_) => spawn_moment)
 );
 
-const subscribe = spawn_times.subscribe((x) => {
+const subscribe = spawn_times.subscribe((spawn_moment) => {
   const canal_test = document.getElementsByClassName("canal_0");
 
   Array.from(canal_test).forEach(function (chanel) {
     const image = document.createElement("img");
     image.src = "azul.png";
-    image.className = "cuadrado_musical";
+
+    const press_moment = spawn_moment + TIME_BEFORE_PRESS;
+    image.className = `cuadrado_musical ${press_moment}`;
 
     chanel.appendChild(image);
 
-    Rx.Observable.timer(TIME_BEFORE_PRESS).subscribe((_) => {
+    Rx.Observable.timer(TIME_BEFORE_DISSAPEAR).subscribe((_) => {
       chanel.removeChild(image);
     });
   });
+});
+
+let first_player_points = 0;
+const first_player_display_points = document.getElementById("puntos");
+function change_fist_player_points(change) {
+  first_player_points += change;
+  first_player_display_points.innerText = `${first_player_points}`;
+}
+
+let second_player_points = 0;
+
+const keyDowns = Rx.Observable.fromEvent(document, "keydown");
+keyDowns.subscribe(function (keyDown) {
+  const time_passed = Date.now() - gameStart;
+  switch (keyDown.key) {
+    case FIRST_PLAYER_BLUE:
+      // IF the difference is lower than the
+      let pointsChange = 0;
+      if (
+        Math.abs(time_passed - currentBluePress.time) <= ERROR_MARGIN &&
+        !currentBluePress.players_pressed[0]
+      ) {
+        pointsChange = 100;
+        currentBluePress.players_pressed[0] = true;
+
+        // remove square
+        const to_remove_square = document
+          .getElementsByClassName("jugador_1")[0]
+          .getElementsByClassName(`${currentBluePress.time}`)[0];
+        console.log(to_remove_square);
+        to_remove_square.parentNode.removeChild(to_remove_square);
+      } else {
+        pointsChange = -100;
+      }
+      change_fist_player_points(pointsChange);
+      break;
+
+    default:
+      break;
+  }
 });
 
 /* let puntos = 0;
