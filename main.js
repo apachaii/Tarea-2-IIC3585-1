5aae4a68c39f8d8ce21663d87568d4ee52a312d4
pablo <perojas3@uc.cl>
2020-04-12 16:59:48 -0400

    Ready with main game

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const AMOUNT_OF_CHANNELS = 2;
 const TIME_BEFORE_PRESS = 4500; // in miliseconds
 const TIME_BEFORE_DISSAPEAR = (TIME_BEFORE_PRESS * 10) / 9; // in miliseconds
 const ERROR_MARGIN = 200; // in miliseconds
+const AVERAGE_BLUE_BY_SECOND = 0.7;
+const AVERAGE_GREEN_BY_SECOND = 0.6;
 
 const FIRST_PLAYER_BLUE = "z";
 const FIRST_PLAYER_GREEN = "x";
 const SECOND_PLAYER_BLUE = "n";
 const SECOND_PLAYER_GREEN = "m";
+
+const FIRST_POINTS_TEXT = "Puntos jugador 1:";
+const SECOND_POINTS_TEXT = "Puntos jugador 2:";
 
 function build_play_area() {
   const juegos = document.getElementsByClassName("juego");
@@ -35,90 +40,165 @@ build_play_area();
 
 // Game started
 
-let gameStart = Date.now();
+let gameStartMoment = 0;
+let gameStarted = false;
 
-const spawn_moments = [1000, 1400, 1800, 2200, 2600, 3000, 3400];
+let currentBluePress = {};
+let currentGreenPress = {};
 
-// Blue press moment
-
-let currentBluePress = { time: 0, players_pressed: [false, false] };
-
-const press_moments_emit = spawn_moments.map((currnt_value, index, array) => {
-  let emit_moment = 0;
-  if (index !== 0) {
-    emit_moment = array[index - 1] + TIME_BEFORE_PRESS + ERROR_MARGIN;
-  }
-  return {
-    emit_moment,
-    press_moment: currnt_value + TIME_BEFORE_PRESS,
-  };
-});
-
-Rx.Observable.from(press_moments_emit)
-  .flatMap((change_press_moment) =>
-    Rx.Observable.timer(change_press_moment.emit_moment).map(
-      (_) => change_press_moment.press_moment
-    )
-  )
-  .subscribe((press_moment) => {
-    currentBluePress = { time: press_moment, players_pressed: [false, false] };
+function getPressEmitArray(circle_spawn_moments) {
+  return circle_spawn_moments.map((currnt_value, index, array) => {
+    let emit_moment = 0;
+    if (index !== 0) {
+      emit_moment = array[index - 1] + TIME_BEFORE_PRESS + ERROR_MARGIN;
+    }
+    return {
+      emit_moment,
+      press_moment: currnt_value + TIME_BEFORE_PRESS,
+    };
   });
-
-const spawn_times = Rx.Observable.from(spawn_moments).flatMap((spawn_moment) =>
-  Rx.Observable.timer(spawn_moment).map((_) => spawn_moment)
-);
-
-const subscribe = spawn_times.subscribe((spawn_moment) => {
-  const canal_test = document.getElementsByClassName("canal_0");
-
-  Array.from(canal_test).forEach(function (chanel) {
-    const image = document.createElement("img");
-    image.src = "azul.png";
-
-    const press_moment = spawn_moment + TIME_BEFORE_PRESS;
-    image.className = `cuadrado_musical ${press_moment}`;
-
-    chanel.appendChild(image);
-
-    Rx.Observable.timer(TIME_BEFORE_DISSAPEAR).subscribe((_) => {
-      chanel.removeChild(image);
-    });
-  });
-});
-
-let first_player_points = 0;
-const first_player_display_points = document.getElementById("puntos");
-function change_fist_player_points(change) {
-  first_player_points += change;
-  first_player_display_points.innerText = `${first_player_points}`;
 }
 
-let second_player_points = 0;
+const changeCurrentBluePress = (pressInfo) => (currentBluePress = pressInfo);
+const changeCurrentGreenPress = (pressInfo) => (currentGreenPress = pressInfo);
 
+function setCurrentPressEmition(emitArray, changePressCallback) {
+  Rx.Observable.from(emitArray)
+    .flatMap((change_press_moment) =>
+      Rx.Observable.timer(change_press_moment.emit_moment).map(
+        (_) => change_press_moment.press_moment
+      )
+    )
+    .subscribe((press_moment) => {
+      changePressCallback({
+        time: press_moment,
+        players_pressed: { jugador_1: false, jugador_2: false },
+      });
+    });
+}
+
+function startCirlesSpawn(spawn_moments, chanel, image_location) {
+  const spawn_times = Rx.Observable.from(
+    spawn_moments
+  ).flatMap((spawn_moment) =>
+    Rx.Observable.timer(spawn_moment).map((_) => spawn_moment)
+  );
+
+  spawn_times.subscribe((spawn_moment) => {
+    const canal_test = document.getElementsByClassName(chanel);
+
+    Array.from(canal_test).forEach(function (chanel) {
+      const image = document.createElement("img");
+      image.src = image_location;
+
+      const press_moment = spawn_moment + TIME_BEFORE_PRESS;
+      image.className = `cuadrado_musical ${press_moment}`;
+
+      chanel.appendChild(image);
+
+      Rx.Observable.timer(TIME_BEFORE_DISSAPEAR).subscribe((_) => {
+        if (image.parentNode) {
+          image.parentNode.removeChild(image);
+        }
+      });
+    });
+  });
+}
+
+function change_points(displayId, default_text) {
+  const display = document.getElementById(displayId);
+  let points = 0;
+
+  function displayPoints() {
+    display.innerText = `${default_text} ${points}`;
+  }
+
+  displayPoints();
+
+  return {
+    changePoints: (change) => {
+      points += change;
+      displayPoints();
+    },
+    resetPoints: () => {
+      points = 0;
+      displayPoints();
+    },
+  };
+}
+
+const first_player_points = change_points("puntaje_1", FIRST_POINTS_TEXT);
+const second_player_points = change_points("puntaje_2", SECOND_POINTS_TEXT);
+
+function manage_key_press(
+  time_passed,
+  player_points,
+  currentPress,
+  currentPlayer,
+  chanel
+) {
+  if (
+    Math.abs(time_passed - currentPress.time) <= ERROR_MARGIN &&
+    !currentPress.players_pressed[currentPlayer]
+  ) {
+    player_points.changePoints(100);
+    currentPress.players_pressed[currentPlayer] = true;
+
+    // remove square
+    const to_remove_square = document
+      .getElementsByClassName(currentPlayer)[0]
+      .getElementsByClassName(chanel)[0]
+      .getElementsByClassName(`${currentPress.time}`)[0];
+    to_remove_square.parentNode.removeChild(to_remove_square);
+  } else {
+    player_points.changePoints(-100);
+  }
+}
+
+// Setting keydowns
 const keyDowns = Rx.Observable.fromEvent(document, "keydown");
 keyDowns.subscribe(function (keyDown) {
-  const time_passed = Date.now() - gameStart;
+  if (!gameStarted) {
+    return;
+  }
+
+  const time_passed = Date.now() - gameStartMoment;
   switch (keyDown.key) {
     case FIRST_PLAYER_BLUE:
-      // IF the difference is lower than the
-      let pointsChange = 0;
-      if (
-        Math.abs(time_passed - currentBluePress.time) <= ERROR_MARGIN &&
-        !currentBluePress.players_pressed[0]
-      ) {
-        pointsChange = 100;
-        currentBluePress.players_pressed[0] = true;
-
-        // remove square
-        const to_remove_square = document
-          .getElementsByClassName("jugador_1")[0]
-          .getElementsByClassName(`${currentBluePress.time}`)[0];
-        console.log(to_remove_square);
-        to_remove_square.parentNode.removeChild(to_remove_square);
-      } else {
-        pointsChange = -100;
-      }
-      change_fist_player_points(pointsChange);
+      manage_key_press(
+        time_passed,
+        first_player_points,
+        currentBluePress,
+        "jugador_1",
+        "canal_0"
+      );
+      break;
+    case FIRST_PLAYER_GREEN:
+      manage_key_press(
+        time_passed,
+        first_player_points,
+        currentGreenPress,
+        "jugador_1",
+        "canal_1"
+      );
+      break;
+    case SECOND_PLAYER_BLUE:
+      manage_key_press(
+        time_passed,
+        second_player_points,
+        currentBluePress,
+        "jugador_2",
+        "canal_0"
+      );
+      break;
+    case SECOND_PLAYER_GREEN:
+      manage_key_press(
+        time_passed,
+        second_player_points,
+        currentGreenPress,
+        "jugador_2",
+        "canal_1"
+      );
       break;
 
     default:
@@ -126,128 +206,89 @@ keyDowns.subscribe(function (keyDown) {
   }
 });
 
-/* let puntos = 0;
-let n = 0;
-let cuadrado = document.getElementById("div1");
-let verde = document.getElementById("div2");
-let rojo = document.getElementById("div3");
-const subject = new Rx.Subject();
-let cambia_tiempo = new Array();
-cambia_tiempo[0] = 1000;
-cambia_tiempo[1] = 3000;
-cambia_tiempo[2] = 5000;
-
-let p = 0;
-
-let red = 0;
-let blue = 0;
-let greeen = 0;
-
-function rand(n) {
-  return Math.floor(Math.random() * n);
+// These functions are taken from here:
+// https://stackoverflow.com/questions/18218256/generate-an-array-of-numbers-from-an-interval-and-keeping-a-minimum-distance-of
+function rand(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
-window.setInterval(function () {
-  p++;
-}, 1000);
+// from:     starting number
+// to:       ending number
+// inc:      distance between each random number
+// amount:   total number of random numbers
+function spacedRandArray(from, to, inc, amount) {
+  var retArray = [rand(from, to)]; // sets the first element
+  var temp = null;
 
-rojo.style.animation = `mymove3`;
-cuadrado.style.animation = `mymove3`;
-verde.style.animation = `mymove3`;
+  for (var x = 0; x < amount - 1; x++) {
+    do {
+      temp = rand(from, to);
+    } while (Math.abs(temp - retArray[x]) <= inc);
 
-window.addEventListener("keypress", function (e) {
-  let jugar = e.key;
-  if (jugar === "j") {
-    red = cambia_tiempo[rand(3)];
-    greeen = cambia_tiempo[rand(3)];
-    blue = cambia_tiempo[rand(3)];
-
-    setTimeout(cuadradoStart, blue);
-    setTimeout(verdeStart, greeen);
-    setTimeout(rojoStart, red);
-
-    window.setInterval(function () {
-      n++;
-    }, 1000);
-
-    setInterval(() => {
-      console.log(n);
-      subject.next(n);
-    }, 1000);
-  } //fin j
-}); //fin 1
-
-function cuadradoStart() {
-  cuadrado.style.animation = ``;
-}
-function verdeStart() {
-  verde.style.animation = ``;
-}
-function rojoStart() {
-  rojo.style.animation = ``;
-}
-
-subject.subscribe((n) => {
-  //cuadrado azul
-  t = 9 + blue / 1000;
-  window.addEventListener("keypress", function (e) {
-    let letra = e.key;
-
-    console.log(n);
-    if (letra === "z" && +n === t) {
-      cuadrado.style.animation = `mymove3`;
-      puntos = puntos + 1;
-      console.log("puntos" + puntos);
-    } //fin z
-  }); //fin 2
-
-  if (n === t) {
-    console.log("paso por aca");
-    cuadrado.style.animation = `mymove3`;
-    puntos = puntos - 10;
+    retArray.push(temp);
   }
-});
 
-subject.subscribe((n) => {
-  //cuadrado verde
-  t = 9 + greeen / 1000;
-  window.addEventListener("keypress", function (e) {
-    let letra = e.key;
+  return retArray;
+}
 
-    console.log(n);
-    if (letra === "x" && +n === t) {
-      verde.style.animation = `mymove3`;
-      puntos = puntos + 1;
-      console.log("puntos" + puntos);
-    } //fin z
-  }); //fin 2
+const play_button = document.getElementById("play");
+const my_audio = document.getElementById("myaudio");
 
-  if (n === t) {
-    console.log("paso por aca");
-    verde.style.animation = `mymove3`;
-    puntos = puntos - 10;
-  }
-});
+function start_game() {
+  // reset game
+  play_button.disabled = true;
+  gameStartMoment = Date.now();
+  gameStarted = true;
+  first_player_points.resetPoints();
+  second_player_points.resetPoints();
 
-subject.subscribe((data) => {
-  //cuadrado rojo
-  t = 9 + red / 1000;
-  window.addEventListener("keypress", function (e) {
-    let letra = e.key;
+  const { duration } = my_audio;
+  const duration_in_milliseconds = Math.ceil(duration * 1000);
 
-    console.log(n);
-    if (letra === "c" && +n === t) {
-      rojo.style.animation = `mymove3`;
-      puntos = puntos + 1;
-      console.log("puntos" + puntos);
-    } //fin z
-  }); //fin 2
+  // Randomly select stars
+  const blue_square_amout = Math.ceil(
+    Math.ceil(duration) * AVERAGE_BLUE_BY_SECOND
+  );
+  const green_square_amout = Math.ceil(
+    Math.ceil(duration) * AVERAGE_GREEN_BY_SECOND
+  );
 
-  if (n === t) {
-    console.log("paso por aca");
-    rojo.style.animation = `mymove3`;
-    puntos = puntos - 10;
-  }
-}); */
+  let spawn_moments_blue = spacedRandArray(
+    0,
+    duration_in_milliseconds,
+    ERROR_MARGIN * 2,
+    blue_square_amout
+  );
+  let spawn_moments_green = spacedRandArray(
+    0,
+    duration_in_milliseconds,
+    ERROR_MARGIN * 2,
+    green_square_amout
+  );
 
-// window.onload = function () {};
+  // Set the starts
+  const blue_emit_array = getPressEmitArray(spawn_moments_blue);
+  setCurrentPressEmition(blue_emit_array, changeCurrentBluePress);
+
+  const green_emit_array = getPressEmitArray(spawn_moments_green);
+  setCurrentPressEmition(green_emit_array, changeCurrentGreenPress);
+
+  // Spawn the circles
+  startCirlesSpawn(spawn_moments_blue, "canal_0", "azul.png");
+  startCirlesSpawn(spawn_moments_green, "canal_1", "verde.png");
+
+  // Start music
+  Rx.Observable.timer(TIME_BEFORE_PRESS).subscribe((_) => {
+    my_audio.play();
+  });
+
+  // stop game after music
+  Rx.Observable.timer(TIME_BEFORE_PRESS + duration_in_milliseconds).subscribe(
+    (_) => {
+      play_button.disabled = false;
+      gameStarted = false;
+    }
+  );
+}
+
+play_button.onclick = start_game;
